ae65bda8580a0f4b38939fa366373e3bd5050435
igor777-cool <igor_baranov@tut.by>
2019-11-13 16:02:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import TodoListHeader from "./TodoListHeader";
+import TodoListTasks from "./TodoListTasks";
+import TodoListFooter from "./TodoListFooter";
+
+class App extends React.Component {
+    render = () => {
+        return (
+            <div className="App">
+                <div className="todoList">
+
+                    <TodoListTasks />
+                    <TodoListHeader />
+                    <TodoListFooter />
+                </div>
+            </div>
+
+
+
+                   /*
+                    */
+
+        );
+    }
+}
+
+export default App;
+
