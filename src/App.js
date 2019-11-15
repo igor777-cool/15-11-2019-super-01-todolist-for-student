aa2a05a70c7b0fea1d22d1a291a2a1531622b0d4
igor777-cool <igor_baranov@tut.by>
2019-11-15 22:04:17 +0300

    I am does to 2 exercise

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
+    tasks = [
+        { title: "CSS", isDone: true, priority: "high"},
+        { title: "HTML", isDone: true, priority: "low" },
+        { title: "React", isDone: false, priority: "high"},
+        { title: "JS", isDone: false, priority: "medium"},
+
+];
+    filterValue = "All"
     render = () => {
+
+
         return (
             <div className="App">
                 <div className="todoList">
-
-                    <TodoListTasks />
                     <TodoListHeader />
-                    <TodoListFooter />
+                    <TodoListTasks tasks={this.tasks} />
+                    <TodoListFooter filterValue = {this.filterValue } />
                 </div>
             </div>
 
--- src/TodoListTasks.js
+++ src/TodoListTasks.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
+import TodoListTask from "./TodoListTask";
 
 class TodoListTasks extends React.Component {
     render = () => {
+
+    let tasksElements=this.props.tasks.map( (t) => {
+        return <TodoListTask title={t.title} isDone={t.isDone} priority={t.priority}/>;
+    } );
+
+
+    /*let tasksElementsFake = [
+        <TodoListTask title={tasks[0].title} isDone={tasks[0].isDone}/>,
+        <TodoListTask title={tasks[1].title}  isDone={tasks[1].isDone}/>,
+        <TodoListTask title={tasks[2].title}  isDone={tasks[2].isDone}/>
+    ]*/
+
+
         return (
             <div className="todoList-tasks">
-                <div className="todoList-task">
-                    <input type="checkbox" checked={true}/>
-                    <span>CSS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={false}/>
-                    <span>JS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={false}/>
-                    <span>ReactJS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={true}/>
-                    <span>Patterns</span>
-                </div>
+                {tasksElements}
             </div>
         );
     }
